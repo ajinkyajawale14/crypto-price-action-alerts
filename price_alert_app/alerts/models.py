10f19735bd4de4f0cba3d2df3d2ea6d44d01b759
ajinkyajawale14 <ajinkyajawale14499@gmail.com>
2023-05-25 14:44:55 +0530

    add send price trigger mail to users

--- price_alert_app/alerts/models.py
+++ price_alert_app/alerts/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+import smtpd
+from django.core.mail import send_mail
+from django.db import models
+
 
 # Create your models here.
 
@@ -10,9 +14,24 @@ class Alert(models.Model):
         ('triggered', 'Triggered'),
     )
 
-user = models.ForeignKey(User, on_delete=models.CASCADE)
-target_price = models.DecimalField(max_digits=10, decimal_places=2)
-status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='created')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    target_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='created')
 
-created_at = models.DateTimeField(auto_now_add=True)
-updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        if self.status == 'created' and self.current_price >= self.target_price:
+            self.status = 'triggered'
+            # send email to users
+            send_mail(
+                'Price Alert!!!',
+                f'price alert for {self.currency} has been triggered. Current price is {self.current_price}',
+                'ajinkya@gmail.com',
+                [self.user.email],
+                fail_silently=False
+            )
+            # print the email
+            print(f'Price alert for {self.currency} triggered. Current price: {self.current_price}')
+            super().save(*args, **kwargs)
